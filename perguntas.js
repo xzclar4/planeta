5b8b4835e6deeb1b3ea2b3983826305ba63ea6b3
xzclar4 <anaclarathavares2041@gmail.com>
2024-11-21 14:18:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Sistema Sola',
+    'O que é Órbita?',
+    'Órbita é a trajetória gravitacionalmente curva de um objeto, como a trajetória de um planeta ao redor de uma estrela ou um satélite natural ao redor de um planeta.'
+)
+
+criaCartao(
+    'Sistema solar',
+    'O que são estrelas?',
+    'São objetos muito facilmente observados e podem ser conhecidas como os “tijolos” na construção das galáxias. '
+)
+
+criaCartao(
+    'Sistema solar',
+    'Como surgiu o universo?',
+    'A teoria do Big Bang descreve a origem do Universo a partir da expansão violenta de uma partícula muito densa e extremamente quente que teve início há 13,8 bilhões de anos, aproximadamente'
+)
+
+criaCartao(
+    'Sistema Solar',
+    'O que é uma estrela cadente?',
+    'Rochas espaciais, chamadas de meteoroides, vagam pelo espaço, em torno do Sol e próximo à Terra'
+)
